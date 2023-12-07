329b0ebe40a30a66eb3f44b7c265082fbf87f540
AlexandreSantanaa <126908528+AlexandreSantanaa@users.noreply.github.com>
2023-12-07 16:43:03 -0300

    pokedex

---
+++ src/javaScript/Script.js
@@ -0,0 +1,59 @@
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_img');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+const fetchpokemon = async (pokemon) =>{
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    if(APIResponse.status===200){
+        const data = await APIResponse.json();
+        return data;
+    }
+   
+}
+
+const renderPokemon = async (pokemon) =>{
+    pokemonName.innerHTML = 'Loanding...';
+
+    const data = await fetchpokemon(pokemon)
+
+    if(data){
+        pokemonImage.style.display ='block'
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        searchPokemon=data.id
+        pokemonNumber.input.value=''
+
+    }else{
+        pokemonImage.style.display ='none'
+        pokemonName.innerHTML="not found :("
+        pokemonNumber.input.value=''
+    }
+    
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+})
+
+buttonNext.addEventListener('click', (event) => {
+    searchPokemon+=1
+    renderPokemon(searchPokemon)
+})
+
+buttonPrev.addEventListener('click', (event) => {
+    searchPokemon-=(searchPokemon>1)?1:0;
+    renderPokemon(searchPokemon)
+})
+
+
+renderPokemon(searchPokemon)
